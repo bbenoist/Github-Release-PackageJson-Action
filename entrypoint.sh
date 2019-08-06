7cee4f4d54a32cc19cfdda97464a862b68cd5e45
Baptist BENOIST <return_0@live.com>
2019-08-06 17:53:36 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,11 @@ if GIT_DIR=.git git rev-parse "${jsonVersion}" >/dev/null 2>&1 ; then
 else
     echo "No tag found for version ${jsonVersion}, creating a new GitHub release..."
     
+    echo ${GITHUB_TOKEN}
+    hub --version
+    if [[ -f $HOME/.config/hub ]]; then cat $HOME/.config/hub ; fi
+    
     # https://github.com/github/hub/issues/2149#issuecomment-496880225
-    export GITHUB_USER=bot
+    # export GITHUB_USER=bot
     hub release create -m "${MESSAGE}" "${jsonVersion}"
 fi
