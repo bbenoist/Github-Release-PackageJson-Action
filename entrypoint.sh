7805e3c010db2eb396fa27c548c8fa57f5d5edf1
Baptist BENOIST <return_0@live.com>
2019-08-06 17:30:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,7 @@ else
     echo "No tag found for version ${jsonVersion}, creating a new GitHub release..."
     
     # https://github.com/github/hub/issues/2149#issuecomment-496880225
-    echo "github.com:\n- user: ${USER}\n  oauth_token: ${GITHUB_TOKEN}\n  protocol: https\n" > ~/.config/hub
-    cat ~/.config/hub
+    echo "github.com:\n- user: ${GITHUB_ACTOR}\n  oauth_token: ${GITHUB_TOKEN}\n  protocol: https\n" > ~/.config/hub
     unset GITHUB_TOKEN
 
     hub release create -m "${MESSAGE}" "${jsonVersion}"
