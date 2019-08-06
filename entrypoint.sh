110171eb2df81ce9353c92bd4d65fdfc5a04971c
Baptist BENOIST <return_0@live.com>
2019-08-06 16:41:05 +0200

    Debug entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ jsonVersion=`jq -r ".version" package.json`
 if GIT_DIR=.git git rev-parse "${jsonVersion}" >/dev/null 2>&1 ; then
     echo "Tag for version ${jsonVersion} already exists. No need to publish a new release."
 else
-    echo "No tag found for version ${jsonVersion}, creating a new GitHub release..."
-    hub release create -t "${GITHUB_REF}" -m "${MESSAGE}" "${jsonVersion}"
+    echo "No tag found for version ${jsonVersion}, creating a new GitHub release on ref ${GITHUB_REF}..."
+    echo hub release create -t "${GITHUB_REF}" -m "${MESSAGE}" "${jsonVersion}"
 fi
